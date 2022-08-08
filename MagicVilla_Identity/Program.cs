6eed17f4ba33588c026cc7a4f81bec9548d42410
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-08-08 15:04:50 -0500

    Section 14 - Setup Identity Server

--- MagicVilla_Identity/Program.cs
+++ MagicVilla_Identity/Program.cs
@@ -1,3 +1,4 @@
+using MagicVilla_Identity;
 using MagicVilla_Identity.Data;
 using MagicVilla_Identity.Models;
 using Microsoft.AspNetCore.Identity;
@@ -22,7 +23,10 @@ builder.Services.AddIdentityServer(options =>
     options.Events.RaiseFailureEvents = true;
     options.Events.RaiseSuccessEvents = true;
     options.EmitStaticAudienceClaim = true;
-});
+}).AddInMemoryIdentityResources(SD.IdentityResources)
+.AddInMemoryApiScopes(SD.ApiScopes)
+.AddInMemoryClients(SD.Cleints).AddAspNetIdentity<ApplicationUser>()
+.AddDeveloperSigningCredential();
 
 
 var app = builder.Build();
