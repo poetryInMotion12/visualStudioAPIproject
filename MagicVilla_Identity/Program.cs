8855fa5b87e4185b2958267aae9eb10001dc7bcd
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-08-08 15:18:12 -0500

    Section 14 - Seed Database with user roles and user claims

--- MagicVilla_Identity/Program.cs
+++ MagicVilla_Identity/Program.cs
@@ -1,5 +1,6 @@
 using MagicVilla_Identity;
 using MagicVilla_Identity.Data;
+using MagicVilla_Identity.IDbInitializer;
 using MagicVilla_Identity.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -13,7 +14,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddRazorPages();
 
 builder.Services.AddIdentityServer(options =>
@@ -41,7 +42,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+SeedDatabase();
 app.UseRouting();
 app.UseIdentityServer();
 app.UseAuthorization();
@@ -51,3 +52,14 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
